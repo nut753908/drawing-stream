eda43f70f8b29030b66c477a58ffeccae4582d8b
nut <nut753908@gmail.com>
2026-01-14 19:02:47 +0900

    adding some apis using hono and drizzle

--- src/server/index-hono.ts
+++ src/server/index-hono.ts
@@ -1,11 +1,42 @@
+import "dotenv/config";
 import { serve } from "@hono/node-server";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/node-postgres";
 import { Hono } from "hono";
+import { todoTable } from "./db/schema";
+
+const db = drizzle(process.env.DATABASE_URL || "");
+
+// TODO: use oRPC
+// TODO: add validations
+
+const todo = new Hono();
+todo.get("/list", async (c) => {
+  const todos = await db.select().from(todoTable);
+  return c.json(todos);
+});
+todo.post("/create", async (c) => {
+  const body = await c.req.json<{ text: string }>();
+  await db.insert(todoTable).values({ text: body.text });
+  return c.text("created");
+});
+todo.post("/update/:id", async (c) => {
+  const id = c.req.param("id");
+  const body = await c.req.json<{ completed: boolean }>();
+  await db
+    .update(todoTable)
+    .set({ completed: body.completed })
+    .where(eq(todoTable.id, id));
+  return c.text("updated");
+});
+todo.post("/delete/:id", async (c) => {
+  const id = c.req.param("id");
+  await db.delete(todoTable).where(eq(todoTable.id, id));
+  return c.text("deleted");
+});
 
 const app = new Hono();
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+app.route("/todo", todo);
 
 serve(
   {
