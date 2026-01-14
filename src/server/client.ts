107c8d20f0600358075e32bb39e788ad9a3cce56
nut <nut753908@gmail.com>
2026-01-14 22:12:53 +0900

    setting up orpc

---
+++ src/server/client.ts
@@ -0,0 +1,16 @@
+import { createORPCClient, onError } from "@orpc/client";
+import { RPCLink } from "@orpc/client/fetch";
+import type { RouterClient } from "@orpc/server";
+import type { router } from "./router";
+
+const link = new RPCLink({
+  url: "http://localhost:3000/rpc",
+  headers: { Authorization: "Bearer token" },
+  interceptors: [
+    onError((error) => {
+      console.error(error);
+    }),
+  ],
+});
+
+export const orpc: RouterClient<typeof router> = createORPCClient(link);
