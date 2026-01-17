b98f9c4ad310df702ec927934d6ab5a7a69ec39d
nut <nut753908@gmail.com>
2026-01-17 15:55:49 +0900

    get the todo list in created_at order

--- src/server/router.ts
+++ src/server/router.ts
@@ -11,27 +11,30 @@ const TodoSchema = z.object({
   id: z.uuid(),
   text: z.string(),
   completed: z.boolean(),
+  created_at: z.iso.datetime(),
 });
 
 export const listTodo = os.handler(async () => {
-  const todos = await db.select().from(todoTable);
+  const todos = await db.select().from(todoTable).orderBy(todoTable.created_at);
   return todos;
 });
 
 export const createTodo = os
-  .input(TodoSchema.omit({ id: true }))
+  .input(TodoSchema.pick({ text: true, completed: true }))
   .handler(async ({ input }) => {
     await db
       .insert(todoTable)
       .values({ text: input.text, completed: input.completed });
   });
 
-export const updateTodo = os.input(TodoSchema).handler(async ({ input }) => {
-  await db
-    .update(todoTable)
-    .set({ text: input.text, completed: input.completed })
-    .where(eq(todoTable.id, input.id));
-});
+export const updateTodo = os
+  .input(TodoSchema.pick({ id: true, text: true, completed: true }))
+  .handler(async ({ input }) => {
+    await db
+      .update(todoTable)
+      .set({ text: input.text, completed: input.completed })
+      .where(eq(todoTable.id, input.id));
+  });
 
 export const deleteTodo = os
   .input(TodoSchema.pick({ id: true }))
