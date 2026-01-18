4a2f359b51e1319b185ae08d55bc66955437bfae
nut <nut753908@gmail.com>
2026-01-18 17:58:06 +0900

    fix drizzle in auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,12 +1,20 @@
+import "dotenv/config";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { account, session, user, verification } from "./db/schema/auth";
 
 const db = drizzle(process.env.DATABASE_URL || "");
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema: {
+      user,
+      session,
+      account,
+      verification,
+    },
   }),
   trustedOrigins: ["http://127.0.0.1:3000"],
   emailAndPassword: {
